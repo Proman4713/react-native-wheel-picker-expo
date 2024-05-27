b442254c07ff79d2f28e89b6cfd0704216fcd243
Proman4713 <85965528+Proman4713@users.noreply.github.com>
2024-05-27 09:55:52 +0300

    Adapted `types.ts` to new changes in `index.tsx`
    - Used tabs instead of spaces in both `index.tsx` and `types.ts` for convenience
    - Added property `color` to prop `selectedStyle` to be utilized inside the `PickerItem` component in `index.tsx`
    - Added two props `opacity` and `noGrad` to be utilized in `index.tsx`

--- src/types.ts
+++ src/types.ts
@@ -2,31 +2,34 @@ import type { FlatListProps } from 'react-native';
 
 export type ItemType = { label: string; value: any };
 export type RenderItemProps = {
-  fontSize: number;
-  label: string;
-  fontColor: string;
-  textAlign: 'center' | 'auto' | 'left' | 'right' | 'justify';
+	fontSize: number;
+	label: string;
+	fontColor: string;
+	textAlign: 'center' | 'auto' | 'left' | 'right' | 'justify';
 };
 
 export interface IViuPickerProps {
-  items: ItemType[];
-  onChange: (item: { index: number; item: ItemType }) => void;
-  initialSelectedIndex?: number;
-  height?: number;
-  width?: any;
-  flatListProps?: Partial<FlatListProps<ItemType>>;
-  backgroundColor?: string;
-  selectedStyle?: {
-    borderColor?: string;
-    borderWidth?: number;
-  };
-  renderItem?: (props: RenderItemProps) => JSX.Element;
-  haptics?: boolean;
+	items: ItemType[];
+	onChange: (item: { index: number; item: ItemType }) => void;
+	initialSelectedIndex?: number;
+	height?: number;
+	width?: any;
+	flatListProps?: Partial<FlatListProps<ItemType>>;
+	backgroundColor?: string;
+	selectedStyle?: {
+		borderColor?: string;
+		borderWidth?: number;
+		color?: string;
+	};
+	renderItem?: (props: RenderItemProps) => JSX.Element;
+	haptics?: boolean;
+	opacity?: number;
+	noGrad?: boolean;
 }
 
 export interface IViuPickerState {
-  selectedIndex: number;
-  itemHeight: number;
-  listHeight: number;
-  data: ItemType[];
+	selectedIndex: number;
+	itemHeight: number;
+	listHeight: number;
+	data: ItemType[];
 }
